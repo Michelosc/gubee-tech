e13f4bac5253073fc464e417ca53b2389a43a1b6
Michel Corrêa <micheloscbc@hotmail.com>
2021-10-13 23:20:25 -0300

    Projeto criado

---
+++ backend/src/main/java/com/gubee/gubtech/GubTechApplication.java
@@ -0,0 +1,13 @@
+package com.gubee.gubtech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GubTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GubTechApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gubee/gubtech/GubTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gubee.gubtech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GubTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
